57d6fa9ca9cef9e390bfabab22748de7dd18b3af
lucatrazzi <lucax88x@gmail.com>
2019-05-20 18:54:53 +0200

    nerdfonts

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 mkdir -p ~/setup-temp
+mkdir -p ~/.config/autostart
 cd ~/setup-temp
 
 UBUNTU_VERSION=19.04
+JETBRAINS_TOOLBOX=jetbrains-toolbox-1.14.5179
 
-echo this installer is for UBUNTU_VERSION
+echo "this installer is for $UBUNTU_VERSION"
 
 if ! [ -x "$(command -v curl)" ]; then
     echo INSTALLING CURL
     apt-get update > /dev/null
-    apt-get install curl > /dev/null
+    apt-get install curl
 else
     echo CURL ALREADY INSTALLED
 fi
@@ -16,20 +18,46 @@ fi
 if ! [ -x "$(command -v git)" ]; then
     echo INSTALLING GIT
     apt-get update > /dev/null
-    apt-get install git-core > /dev/null
+    apt-get install git-core
     
     wget https://raw.githubusercontent.com/lucax88x/configs/master/.gitconfig -O ~/.gitconfig
 else
     echo GIT ALREADY INSTALLED
 fi
 
+if ! [ "$(fc-list | grep -c 'firacode')" -ge 1 ]; then
+    echo INSTALLING FIRACODE
+    apt install fonts-firacode
+else
+    echo FIRACODE ALREADY INSTALLED
+fi
+
+if ! [ "$(fc-list | grep -c 'nerdfont-complete')" -ge 1 ]; then
+    echo INSTALLING NERDFONT
+
+    git clone https://github.com/ryanoasis/nerd-fonts ~/setup-temp/nerd-fonts
+
+    sh ~/setup-temp/nerd-fonts/install.sh
+else
+    echo NERDFONT ALREADY INSTALLED
+fi
+
+if ! [ -x "$(command -v lsd)" ]; then
+    echo INSTALLING LSD
+    snap install lsd --classic
+else
+    echo LSD ALREADY INSTALLED
+fi
+
 if ! [ -x "$(command -v zsh)" ]; then
     echo INSTALLING ZSH WITH OH-MY-ZSH
     apt-get update > /dev/null
-    apt-get install zsh > /dev/null
+    apt-get install zsh
     
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed '/\s*env\s\s*zsh\s*/d')" \
     
+
+    chmod a+x /usr/bin/chsh
     chsh -s $(which zsh)
     
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
@@ -38,7 +66,7 @@ if ! [ -x "$(command -v zsh)" ]; then
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k\/powerlevel9k"\nPOWERLEVEL9K_DISABLE_RPROMPT=false\nPOWERLEVEL9K_PROMPT_ON_NEWLINE=true\nPOWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="λ "\nPOWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""\nPOWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)\nPOWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)\nPOWERLEVEL9K_MODE="nerdfont-complete"/g' ~/.zshrc
     sed -i 's/plugins=(git)/plugins=(git colored-man-pages zsh-autosuggestions zsh-syntax-highlighting zsh-better-npm-completion yarn-completion)/g' ~/.zshrc
     
- cat <<EOT >> ~/.zshrc
+cat <<EOT >> ~/.zshrc
 alias ls='lsd'
 alias l='ls -l'
 alias la='ls -a'
@@ -47,6 +75,7 @@ alias lt='ls --tree'
 alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 alias repo='f() { cd ~/repos/$1 };f'
 EOT
+
     
 else
     echo ZSH ALREADY INSTALLED
@@ -57,16 +86,9 @@ if ! [ -x "$(command -v google-chrome)" ]; then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
     sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     apt-get update > /dev/null
-    apt-get install google-chrome-stable > /dev/null
+    apt-get install google-chrome-stable
 else
     echo CHROME ALREADY INSTALLED
-fi
-
-if ! [ "$(fc-list | grep -c 'firacode')" -ge 1 ]; then
-    echo INSTALLING FIRACODE
-    apt install fonts-firacode
-else
-    echo FIRACODE ALREADY INSTALLED
 fi
 
 if ! [ -x "$(command -v code)" ]; then
@@ -78,12 +100,10 @@ fi
 
 if ! [ -f ~/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox ]; then
     echo INSTALLING JETBRAINS TOOLBOX
-    echo todo
     
-    #NAME=jetbrains-toolbox-1.14.5179
-    #wget https://download.jetbrains.com/toolbox/$NAME.tar.gz -q --show-progress
-    #tar xvzf $NAME.tar.gz>/dev/null
-    #./$NAME/jetbrains-toolbox>/dev/null
+    wget https://download.jetbrains.com/toolbox/$NAME.tar.gz -q --show-progress
+    tar xvzf $NAME.tar.gz>/dev/null
+    ./$NAME/jetbrains-toolbox>/dev/null
 else
     echo JETBRAINS TOOLBOX ALREADY INSTALLED
 fi
@@ -95,7 +115,15 @@ if ! [ -x "$(command -v albert)" ]; then
     
     sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$UBUNTU_VERSION/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
     apt-get update > /dev/null
-    apt-get install albert > /dev/null
+    apt-get -y install albert
+
+cat <<EOT >> ~/.config/autostart/albert
+[Desktop Entry]
+Name=albert
+Exec=albert
+Type=Application
+EOT
+
 else
     echo ALBERT ALREADY INSTALLED
 fi
@@ -103,9 +131,9 @@ fi
 if [ $(dpkg-query -W -f='${Status}' telegram 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo INSTALLING TELEGRAM
     
-    add-apt-repository ppa:atareao/telegram
+    add-apt-repository -y ppa:atareao/telegram
     apt-get update > /dev/null
-    apt-get install telegram > /dev/null
+    apt-get install telegram
     
 else
     echo TELEGRAM ALREADY INSTALLED
@@ -114,9 +142,9 @@ fi
 if ! [ -x "$(command -v diodon)" ]; then
     echo INSTALLING DIODON
     
-    add-apt-repository ppa:diodon-team/stable
+    add-apt-repository -y ppa:diodon-team/stable
     apt-get update > /dev/null
-    apt-get -y install diodon > /dev/null
+    apt-get -y install diodon
     
 else
     echo DIODON ALREADY INSTALLED
@@ -128,7 +156,6 @@ cd ~
 
 echo Purged temp folder
 
-# LS extend
-# nerdfonts
 # add g as alias to zshrc
+# add git aliases to .gitconfig
 # auto start apps, albert, diodon
