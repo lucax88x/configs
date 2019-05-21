72f32de9f0701a96531e21680d7d9189e8ddd486
lucatrazzi <luca.trazzi@swisscom.com>
2019-05-21 11:19:09 +0200

    tuning powerline

--- install.sh
+++ install.sh
@@ -43,10 +43,10 @@ if ! [ "$(fc-list | grep -c 'PowerlineSymbols')" -ge 1 ]; then
     wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
     wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
     
-	mkdir -p ~/.local/share/fonts/
+    mkdir -p ~/.local/share/fonts/
     mv PowerlineSymbols.otf ~/.local/share/fonts/
     fc-cache -vf ~/.local/share/fonts/
-	mkdir -p ~/.config/fontconfig/conf.d
+    mkdir -p ~/.config/fontconfig/conf.d
     mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
     
 else
