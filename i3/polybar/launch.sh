9fa2f8b6a56c02ef1de3d5a08ef247a4cce9908e
lucatrazzi <luca.trazzi@garaio.com>
2019-11-19 18:17:42 +0100

    more tweaks

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -6,8 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top
-polybar bottom
+polybar laptop & polybar center & polybar right &
 
 echo "Bars launched..."
