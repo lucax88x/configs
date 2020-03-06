64a031a5fa9ebfc100a8a98296e039633d3f5cea
lucatrazzi <luca.trazzi@garaio.com>
2020-03-06 11:50:49 +0100

    fix

--- helpers/sync.sh
+++ helpers/sync.sh
@@ -14,19 +14,19 @@ if [ "$CONT" = "n" ]; then
 fi
 
 declare -A pairs=(
-    # [~/.config/i3/config]=./dotfiles/.config/i3/config
-    # [~/.config/polybar/colors]=./dotfiles/.config/polybar/colors
-    # [~/.config/polybar/config]=./dotfiles/.config/polybar/config
-    # [~/.config/polybar/launch.sh]=./dotfiles/.config/polybar/launch.sh
-    # [~/.config/rofi/config.rasi]=./dotfiles/.config/rofi/config.rasi
-    # [~/.config/rofi/options_menu.rasi]=./dotfiles/.config/rofi/options_menu.rasi
-    # [~/.config/rofi/confirm_menu.rasi]=./dotfiles/.config/rofi/confirm_menu.rasi
-    # [~/.config/dunst/dunstrc]=./dotfiles/.config/dunst/dunstrc
-    # [~/.config/kitty/kitty.conf]=./dotfiles/.config/kitty/kitty.conf
-    # [~/.config/picom/picom.conf]=./dotfiles/.config/picom/picom.conf
-    # [~/.conkyrc]=./dotfiles/.conkyrc
-    # [~/.gitconfig]=./dotfiles/.gitconfig
-    # [~/.Xmodmap]=./dotfiles/.Xmodmap
+    [~/.config/i3/config]=./dotfiles/.config/i3/config
+    [~/.config/polybar/colors]=./dotfiles/.config/polybar/colors
+    [~/.config/polybar/config]=./dotfiles/.config/polybar/config
+    [~/.config/polybar/launch.sh]=./dotfiles/.config/polybar/launch.sh
+    [~/.config/rofi/config.rasi]=./dotfiles/.config/rofi/config.rasi
+    [~/.config/rofi/options_menu.rasi]=./dotfiles/.config/rofi/options_menu.rasi
+    [~/.config/rofi/confirm_menu.rasi]=./dotfiles/.config/rofi/confirm_menu.rasi
+    [~/.config/dunst/dunstrc]=./dotfiles/.config/dunst/dunstrc
+    [~/.config/kitty/kitty.conf]=./dotfiles/.config/kitty/kitty.conf
+    [~/.config/picom/picom.conf]=./dotfiles/.config/picom/picom.conf
+    [~/.conkyrc]=./dotfiles/.conkyrc
+    [~/.gitconfig]=./dotfiles/.gitconfig
+    [~/.Xmodmap]=./dotfiles/.Xmodmap
     [~/bin/]=./scripts/
 )
 
@@ -41,11 +41,9 @@ function sync()
     if [ -d "$FROM" ]; then  
         mkdir -p $TO
         rsync -auv $FROM $TO
-        # cp -vaR $FROM/* $TO
     else
         mkdir -p $TO_DIR
         rsync -auv $FROM $TO
-        # cp -vaR $FROM $TO
     fi
 }
 
