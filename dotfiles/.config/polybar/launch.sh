7ad19f80422024b7d8a9da9015a1ba2b9f7f522e
lucatrazzi <luca.trazzi@garaio.com>
2021-05-04 17:49:20 +0200

    added home desktop monitor

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -6,4 +6,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar laptop & polybar left & polybar center & polybar right & polybar home
+polybar laptop & polybar left & polybar center & polybar right & polybar home & polybar home_desktop
