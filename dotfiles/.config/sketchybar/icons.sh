76eb883ca75b04da758d2d7316f0b6fa8331eeaa
lucatrazzi <luca.trazzi@garaio.com>
2022-10-14 19:21:26 +0200

    added sketchyrc

---
+++ dotfiles/.config/sketchybar/icons.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# General Icons
+export LOADING=􀖇
+export APPLE=􀣺
+export PREFERENCES=􀺽
+export ACTIVITY=􀒓
+export LOCK=􀒳
+export MAIL=􀍜
+export MAIL_OPEN=􀍜
+export BELL=􀋚
+export BELL_DOT=􀝗
+
+# Git Icons
+export GIT_ISSUE=􀍷
+export GIT_DISCUSSION=􀒤
+export GIT_PULL_REQUEST=􀙡
+export GIT_COMMIT=􀡚
+export GIT_INDICATOR=􀂓
+
+# Yabai Icons
+export YABAI_STACK=􀏭
+export YABAI_FULLSCREEN_ZOOM=􀏜
+export YABAI_PARENT_ZOOM=􀥃
+export YABAI_FLOAT=􀢌
+export YABAI_GRID=􀧍
